87f5253cd7ede0bf6a8252a6c4c83f00452e2e29
David <davidresca9@gmail.com>
2025-07-21 13:59:59 -0300

    Agrega listar Inscripciones (con nombres)

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -49,6 +49,7 @@ void GestionarProceso::mostrarMenu() {
     cout << "\n--- PROCESOS COMPLETOS ---" << endl;
     cout << "1. Realizar una Inscripcion" << endl;
     cout << "2. Dar de baja una Inscripcion" << endl;
+    cout << "3. Ver listado de Inscripciones Activas" << endl;
     cout << "0. Volver al Menu Principal" << endl;
 }
 
@@ -56,13 +57,17 @@ void GestionarProceso::iniciar() {
     int opcion;
     do {
         mostrarMenu();
-        opcion = Validaciones::pedirEntero("Seleccione una opcion: ", 0, 2);
+        opcion = Validaciones::pedirEntero("Seleccione una opcion: ", 0, 3);
         switch (opcion) {
             case 1:
                 realizarInscripcion();
                 break;
             case 2:
                 realizarBajaInscripcion();
+                break;
+            case 3:
+                listarInscripciones();
+                system("pause");
                 break;
             case 0:
                 cout << "Volviendo al menu principal..." << endl;
